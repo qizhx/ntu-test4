3227b9702589366bf601e38c4a712c01380b97a8
qizhx <106426046+qizhx@users.noreply.github.com>
2024-07-24 10:14:27 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask,render_template,request
+import google.generativeai as palm
+ 
+palm.configure(api_key="")
+model = {"model":"models/chat-bison-001"}
+app = Flask(__name__)
+ 
+@app.route("/",methods=["GET","POST"])
+def index():
+    return(render_template("index.html"))
+ 
+@app.route("/main",methods=["GET","POST"])
+def main():
+    r = request.form.get("q")
+    return(render_template("main.html",r=r))
+ 
+@app.route("/genAI",methods=["GET","POST"])
+def genAI():
+    q = request.form.get("q")
+    r = palm.chat(**model,messages=q)
+    return(render_template("genAI.html",r=r.last))
+ 
+if __name__ == "__main__":
+    app.run()
